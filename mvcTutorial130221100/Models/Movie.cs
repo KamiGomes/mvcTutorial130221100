65cfaa13ba74e9cfb834f20b9639f747b1b2ae76
Miguel Gomes <gomesmike24@hotmail.com>
2018-10-10 11:36:17 +0100

    Commit - Controller methods and views

--- mvcTutorial130221100/Models/Movie.cs
+++ mvcTutorial130221100/Models/Movie.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace mvcTutorial130221100.Models
 {
@@ -11,8 +9,11 @@ namespace mvcTutorial130221100.Models
         public int ID { get; set; }
         public string Title { get; set; }
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
